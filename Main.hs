fb0faa790f3e9b5662729bc230b6c9f9751729ab
mmhl <mmhl@users.noreply.github.com>
2015-07-10 17:19:57 +0200

    Added option functions stubs and descriptions. Created task data structure, witch uses ZonedTime

--- Main.hs
+++ Main.hs
@@ -1,3 +1,58 @@
 import System.Environment
+import Data.Time
+import Control.Applicative
 
-main = do putStrLn "Hello, old cabal"
+
+type TaskTitle = String
+type TaskDescription = String
+type TaskCreationDate = ZonedTime
+data Task = Task {  title :: TaskTitle
+                    , description :: TaskDescription
+                    , dateCreated :: TaskCreationDate
+                 } deriving (Show)
+
+newTask :: TaskTitle -> TaskDescription -> TaskCreationDate -> Task
+newTask t d date = Task t d date
+
+optionNew :: IO ()
+optionNew = do putStrLn "New entry"
+
+optionNewDescription = "Create new entry"
+
+optionView :: IO ()
+optionView = do putStrLn "View entries"
+optionViewDescription = "View entries"
+
+optionDelete :: IO ()
+optionDelete = do putStrLn "Entry delete"
+optionDeleteDescription = "Delete entry"
+
+optionError :: IO ()
+optionError = do error "Invalid Option"
+
+optionList = [optionNew, optionView, optionDelete, optionError]
+optionDescriptionList = [optionNewDescription, optionViewDescription, optionDeleteDescription]
+optionNumbers = [1..]
+
+options = zip3 optionNumbers optionDescriptionList optionList
+
+getOptionNumber :: (Integer, String, IO () ) -> Integer
+getOptionNumber (x, _, _) = x
+
+getOptionDescription :: (Integer, String, IO () ) -> String
+getOptionDescription (_, x, _) = x
+
+getOptionFunction :: (Integer, String, IO () ) -> (IO ())
+getOptionFunction (_, _, fn) = fn
+
+
+printOptions :: IO ()
+printOptions = do
+    mapM_ (\(num, desc, _) -> putStrLn (line num desc )) options
+    where line n desc = (show n) ++ ". " ++ desc
+
+
+main = do
+    c <- getZonedTime
+    let x = newTask "Test" "This is a test description" c
+    putStrLn (show x)
